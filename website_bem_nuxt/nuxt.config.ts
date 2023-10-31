c5de7cc546faf9e12765cec5471eba9dd0c5ace4
puzul <ymjonathangp2@gmail.com>
2023-10-31 22:20:07 +0700

    V0.9.3 Patch
    Secret API key (removed)

--- website_bem_nuxt/nuxt.config.ts
+++ website_bem_nuxt/nuxt.config.ts
@@ -11,7 +11,6 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: ['vue3-carousel-nuxt', "@nuxt/image"],
   nitro: {
     firebase: {
       gen: 2,
@@ -30,19 +29,9 @@ export default defineNuxtConfig({
       ]
     }
   },
+  modules: ['vue3-carousel-nuxt', "@nuxt/image"],
   runtimeConfig: {
-    FB_TYPE: process.env.FB_TYPE,
-    FB_PRIVATE_KEY_ID: process.env.FB_PRIVATE_KEY_ID,
-    FB_PRIVATE_KEY: process.env.FB_PRIVATE_KEY,
-    FB_CLIENT_EMAIL: process.env.FB_CLIENT_EMAIL,
-    FB_CLIENT_ID: process.env.FB_CLIENT_ID,
-    FB_AUTH_URI: process.env.FB_AUTH_URI,
-    FB_TOKEN_URI: process.env.FB_TOKEN_URI,
-    FB_AUTH_PROVIDER_X509_CERT_URL: process.env.FB_AUTH_PROVIDER_X509_CERT_URL,
-    FB_CLIENT_X509_CERT_URL: process.env.FB_PRIVATE_KEY_ID,
-    FB_UNIVERSE_DOMAIN: process.env.FB_UNIVERSE_DOMAIN,
     public: {
-      PASSWORD: process.env.PASSWORD,
       FB_API_KEY: process.env.FB_API_KEY,
       FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
       FB_PROJECT_ID: process.env.FB_PROJECT_ID,

e235a22bdbee8ce9865a9fbd6c6a183652185e89
puzul <ymjonathangp2@gmail.com>
2023-11-24 17:13:25 +0700

    V0.11.1-E-NV Patch

--- website_bem_nuxt/app.vue
+++ website_bem_nuxt/app.vue
@@ -34,6 +34,7 @@
   
   onMounted(() => {
     const { analytics } = useFirebase();
+    alert('Anda sedang mengunjungi Website Resmi BEM KM-UAJ V0.11.1-E. Website ini sedang dalam tahap open beta dan BUKAN merupakan versi full. Apabila Anda menemukan kendala atau bug atau memiliki saran, silakan menghubungi Instagram @bem_kmuaj atau menggunakan form pada halaman /contact-us. Selamat explorasi!');
   })
     
 </script>

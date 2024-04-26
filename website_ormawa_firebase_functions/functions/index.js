d82457de30e9aa8fbd8a4dc7fa7d405b20d18a96
puzul <ymjonathangp2@gmail.com>
2024-04-26 12:53:12 +0700

    V1.0.3 Patch
    Firebase functions delete vote value after vote logic (removed)
    Firebase function runtime Node.js 20 (updated)

--- website_ormawa_firebase_functions/functions/index.js
+++ website_ormawa_firebase_functions/functions/index.js
@@ -27,7 +27,7 @@ setGlobalOptions({ region: 'asia-southeast2' });
 const db = getFirestore();
 
 
-const checkTextFormat = (textSplittedCleaned) => {
+const checkTextFormat = (textSplittedCleaned) => { // Function for checking student card text format
   const firstLineIsValid = textSplittedCleaned[0]==='UNIVERSITAS KATOLIK INDONESIA' ? true : false;
   const SecondtLineIsValid = textSplittedCleaned[1]==='ATMA JAYA' ? true : false;
   const FifthLineIsValid = textSplittedCleaned[4]==='www.atmajaya.ac.id | Telp: +62215703306' ? true : false;
@@ -37,7 +37,7 @@ const checkTextFormat = (textSplittedCleaned) => {
   else return false;
 }
 
-const returnCampusID = (textSplittedCleaned) => {
+const returnCampusID = (textSplittedCleaned) => { // Function for returning campus ID from student card text array
   let campusID = '';
   textSplittedCleaned[2].split(' ').forEach((item) => {
     campusID = campusID + item;
@@ -45,7 +45,7 @@ const returnCampusID = (textSplittedCleaned) => {
   return campusID;
 }
 
-const returnEmailUsername = (textSplittedCleaned) => {
+const returnEmailUsername = (textSplittedCleaned) => { // Functionn for returning email username from student card text array
   const campusID = returnCampusID(textSplittedCleaned);
   const firstName = textSplittedCleaned[3].split(' ')[0].toLowerCase();
   const first7Letters = firstName.length <= 7 ? firstName : firstName.substring(0,7);
@@ -53,7 +53,8 @@ const returnEmailUsername = (textSplittedCleaned) => {
   return emailUsername;
 }
 
-export const votingValidator = onObjectFinalized(async (object) => {
+// --Start votingValidator Function--
+export const votingValidator = onObjectFinalized(async (object) => { // Function triggered when a new file uploaded to the storage bucket
   const data = object.data;
   const fileBucket = data.bucket;
   const filePath = data.name;
@@ -63,7 +64,7 @@ export const votingValidator = onObjectFinalized(async (object) => {
 
   let isValid = false;
 
-  // First validation:
+  // --Start first validation--
   // Check if the file extension is jpg or jpeg and the file is in the student_card_images folder
   // (Cannot be seen in the front end)
   if ((contentType==='image/jpg' || contentType==='image/jpeg') && fileDir==='student_card_images') {
@@ -73,20 +74,20 @@ export const votingValidator = onObjectFinalized(async (object) => {
     const [annotation] = textDetection.textAnnotations;
     const text = annotation ? annotation.description : null;
 
-    // Second validation:
+    // --Start second validation--
     // Check if there is any text extracted from the image
     // Can be seen in the front end: Code 5 (if text is null)
     if (text) {
       const textSplitted = text.split(/\r?\n|\r|\n/g);
 
-      // Third validation:
+      // --Start third validation--
       // Check if the text array is either has a length of 7 or 8 (since sometimes there is one string item that is false detected in the beginning of the array)
       // Can be seen in the front end: Code 5 (if text array length is less than 7 or more than 8)
       if (textSplitted.length>=8 && textSplitted.length <=9) {
         const textSplittedCleaned = textSplitted[0]==='UNIVERSITAS KATOLIK INDONESIA' ? textSplitted : textSplitted.slice(1);
         const textFormatIsValid = checkTextFormat(textSplittedCleaned);
 
-        // Fourth validation:
+        // --Start fourth validation--
         // Check if the text format from the image text extraction match the official student card text format 
         // Can be seen in the front end: Code 4 (if text format don't match the official format)
         if (textFormatIsValid) {
@@ -94,13 +95,13 @@ export const votingValidator = onObjectFinalized(async (object) => {
           const [faceDetection] = await client.faceDetection(`gs://${fileBucket}/${filePath}`);
           const faces = faceDetection.faceAnnotations;
 
-          // Fifth validation:
+          // --Start fifth validation--
           // Check if there is at least one and only one face detected from the image face extraction  
           // Can be seen in the front end: Code 4 (if there is no face detected) or Code 3 (if there is more than one face detected)
           if (faces.length===1) {
             db.collection('voter_emails').doc(emailDocID).get().then(snap => {
 
-              // Sixth validation:
+              // --Start sixth validation--
               // Check if there is the document with the same ID as the file name existed
               // Cannot be seen in the front end
               if (snap.exists) {
@@ -108,13 +109,13 @@ export const votingValidator = onObjectFinalized(async (object) => {
 
                 const email = snap.data().email;
 
-                // Seventh validation:
+                // --Start seventh validation--
                 // Check if the email username from the image text extraction match the email from the user input
                 // Can be seen in the front end: Code 2 (if the email username from the extraction don't match the email from the user input)
                 if (email.split('@')[0]===emailUsername) {
                   db.collection('voted_server').doc(emailUsername).get().then(snap => {
 
-                    // Eight validation:
+                    // --Start eight validation--
                     // Check if the voter already voted
                     // Can be seen in the front end: Code 1 (if the voter already voted before)
                     if (!snap.exists) {
@@ -130,31 +131,34 @@ export const votingValidator = onObjectFinalized(async (object) => {
                       }).then(() => logger.log(emailDocID, '0'));
                     }
                     else {
-                      db.collection('voter_votes').doc(emailDocID).delete();
                       db.collection('voted_clients').doc(emailDocID).set({
                         isValid: isValid,
                         code: '1',
                         timeCreated: FieldValue.serverTimestamp(),
                       }).then(() => logger.log(emailDocID, '1'));
                     }
+                    // --End eight validation--
+
                   })
                 }
                 else {
-                  db.collection('voter_votes').doc(emailDocID).delete();
                   db.collection('voted_clients').doc(emailDocID).set({
                     isValid: isValid,
                     code: '2',
                     timeCreated: FieldValue.serverTimestamp(),
                   }).then(() => logger.log(emailDocID, '2', email, emailUsername));
                 }
+                // --End seventh validation--
+
               }
               else {
                 logger.log(emailDocID, `Firebase document based on email doc ID doesn't exist`);
               }
+              // --End sixth validation--
+
             })
           }
           else if (faces.length>=2) {
-            db.collection('voter_votes').doc(emailDocID).delete();
             db.collection('voted_clients').doc(emailDocID).set({
               isValid: isValid,
               code: '3',
@@ -162,140 +166,54 @@ export const votingValidator = onObjectFinalized(async (object) => {
             }).then(() => logger.log(emailDocID, '3', faces));
           }
           else {
-            db.collection('voter_votes').doc(emailDocID).delete();
             db.collection('voted_clients').doc(emailDocID).set({
               isValid: isValid,
               code: '4',
               timeCreated: FieldValue.serverTimestamp(),
             }).then(() => logger.log(emailDocID, '4', faces));
           }
+          // --End fifth validation--
+
         }
         else {
-          db.collection('voter_votes').doc(emailDocID).delete();
           db.collection('voted_clients').doc(emailDocID).set({
             isValid: isValid,
             code: '4',
             timeCreated: FieldValue.serverTimestamp(),
           }).then(() => logger.log(emailDocID, '4', textSplitted, textSplittedCleaned));
         }
+        // --End fourth validation--
+
       }
       else {
-        db.collection('voter_votes').doc(emailDocID).delete();
         db.collection('voted_clients').doc(emailDocID).set({
           isValid: isValid,
           code: '5',
           timeCreated: FieldValue.serverTimestamp(),
         }).then(() => logger.log(emailDocID, '5', textSplitted));
       }
+      // --End third validation--
+
     }
     else {
-      db.collection('voter_votes').doc(emailDocID).delete();
       db.collection('voted_clients').doc(emailDocID).set({
         isValid: isValid,
         code: '5',
         timeCreated: FieldValue.serverTimestamp(),
       }).then(() => logger.log(emailDocID, '5', text));
     }
+    // --End second validation--
+
   }
   else {
     logger.log(emailDocID, `File file type is not jpg or jpeg`);
   }
+  // --End first validation--
 
 })
+// --End votingValidator Function--
 
-// export const votingValidator = onDocumentCreated("student_card_texts/{docID}", async (event) => {
-//   const snapshot = event.data;
-//   const data = snapshot.data();
-
-//   const file = data.file;
-//   const emailDocID = file.split('/')[4].split('.')[0];
-
-//   let isValid = false;
-
-//   if (data.text) {
-//     const textSplitted = data.text.split(/\r?\n|\r|\n/g);
-    
-//     if (textSplitted.length >= 7) {
-//       const textCredentialsIsValid = checkTextFormat(textSplitted);
-
-//       // First Validation Function checking if the student card is valid
-      
-//       if (firstLineIsValid && SecondtLineIsValid && FifthLineIsValid && SixthLineIsValid && EighthLineIsValid) {
-
-//         //Second Validation Function checking if the information from the student card matches the student email
-        
-//         const campusID = returnCampusID();
-//         const firstName = textSplittedCleaned[3].split(' ')[0].toLowerCase();
-//         const first7Letters = firstName.length <= 7 ? firstName : firstName.substring(0,7);
-//         const emailUsername = `${first7Letters}.${campusID}`;
-      
-//         db.collection('voter_emails').doc(emailDocID).get().then(snap => {
-//           const email = snap.data().email;
-//           if (email.split('@')[0]===emailUsername) {
-      
-//             //Third Validation Function checking if the voter already voted based on the student email username information
-//             db.collection('voted_server').doc(emailUsername).get().then(snap => {
-//               if (!snap.exists) {
-//                 isValid = true;
-//                 db.collection('voted_server').doc(emailUsername).set({
-//                   emailDocID: emailDocID,
-//                   timeCreated: FieldValue.serverTimestamp(),
-//                 });
-//                 db.collection('voted_clients').doc(emailDocID).set({
-//                   isValid: isValid,
-//                   code: '0',
-//                   timeCreated: FieldValue.serverTimestamp(),
-//                 }).then(() => logger.log(emailDocID, '0'));
-//                 logger.log(emailDocID, '0', emailUsername);
-//               }
-//               else {
-//                 db.collection('voter_votes').doc(emailDocID).delete();
-//                 db.collection('voted_clients').doc(emailDocID).set({
-//                   isValid: isValid,
-//                   code: '1',
-//                   timeCreated: FieldValue.serverTimestamp(),
-//                 }).then(() => logger.log(emailDocID, '1'));
-//               }
-//             })
-//           }
-//           else {
-//             db.collection('voter_votes').doc(emailDocID).delete();
-//             db.collection('voted_clients').doc(emailDocID).set({
-//               isValid: isValid,
-//               code: '2',
-//               timeCreated: FieldValue.serverTimestamp(),
-//             }).then(() => logger.log(emailDocID, '2', email, emailUsername, textSplitted, textSplittedCleaned));
-//           }
-//         })
-//       }
-//       else {
-//         db.collection('voter_votes').doc(emailDocID).delete();
-//         db.collection('voted_clients').doc(emailDocID).set({
-//           isValid: isValid,
-//           code: '3',
-//           timeCreated: FieldValue.serverTimestamp(),
-//         }).then(() => logger.log(emailDocID, '3', textSplitted, textSplittedCleaned));
-//       }
-//     }
-//     else {
-//       db.collection('voter_votes').doc(emailDocID).delete();
-//       db.collection('voted_clients').doc(emailDocID).set({
-//         isValid: isValid,
-//         code: '3',
-//         timeCreated: FieldValue.serverTimestamp(),
-//       }).then(() => logger.log(emailDocID, '3', textSplitted));
-//     }
-//   }
-//   else {
-//     db.collection('voter_votes').doc(emailDocID).delete();
-//     db.collection('voted_clients').doc(emailDocID).set({
-//       isValid: isValid,
-//       code: '4',
-//       timeCreated: FieldValue.serverTimestamp(),
-//     }).then(() => logger.log(emailDocID, '4', data.text));
-//   }
-// })
-
+// --Start voteCounter Function--
 export const voteCounter = onDocumentCreated("voted_server/{docID}", (event) => {
   const snapshot = event.data
   const data = snapshot.data();
@@ -304,21 +222,18 @@ export const voteCounter = onDocumentCreated("voted_server/{docID}", (event) => 
     const voteValue = snap.data().voteValue;
 
     if (voteValue==='1') {
-      db.collection("voted_counter").doc('5vPVTTOox1Nhxdy3LUhE').update({
+      db.collection("voted_counter").doc('vote_count').update({
         "candidatesNumber1.voters" : FieldValue.increment(1),
-      }).then(() => {
-        db.collection("voter_votes").doc(data.emailDocID).delete();
       })
     }
     else if (voteValue==='2') {
-      db.collection("voted_counter").doc('5vPVTTOox1Nhxdy3LUhE').update({
+      db.collection("voted_counter").doc('vote_count').update({
         "candidatesNumber2.voters" : FieldValue.increment(1),
-      }).then(() => {
-        db.collection("voter_votes").doc(data.emailDocID).delete();
       })
     }
   })
 })
+// --End voteCounter Function--
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
